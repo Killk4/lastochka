acc75c353c9c1be949ac93ad1a23d75c202a7557
Borisevich V <vasily.borisevich@gmail.com>
2023-03-22 14:32:30 +0300

    Новая версия шредера. Работает со всеми файлами.

--- sandbox.py
+++ sandbox.py
@@ -1,22 +1,49 @@
+'''Пока что лючший шредер из найденных
+Надо разобрать код и внедрить в существующий
+https://github.com/alec-jensen/file-shredder
+Было заменено sys.argv[1] на статичное имя файла'''
+
+
 import os
-import random
+from termcolor import colored
+import colorama
+
+colorama.init() # Инициализация colorama для работы с цветным текстом в консоли
+success = True # Глобальная переменная для отслеживания успешности выполнения операций
 
 
-def delete_file(file_path):
-    '''Функция удаления файла.
-    Принимает путь к файлу, который нужно удалить
-    Функция открывает файл в режиме ba+, что позволяет открыть файл для чтения и записи в бинарном режиме.
-    Затем функция определяет размер файла и перезаписывает его случайными байтами.
-    После этого функция удаляет файл с диска.'''
+def secure_delete(path, passes=5):
+    global success # Использование глобальной переменной success
+    with open(path, "ba+") as delfile:
+        length = delfile.tell() # Определение размера файла
+    with open(path, "br+") as delfile:
+        for i in range(passes): # Перезапись содержимого файла случайными данными passes раз
+            try:
+                delfile.seek(0) # Перемещение указателя в начало файла
+                delfile.write(os.urandom(length)) # Запись случайных данных в файл
+                print(colored(f'ПРОХОД {i + 1} УСПЕШНО', 'green')) # Вывод сообщения об успешном прохождении итерации
+            except: # Обработка исключений при записи в файл
+                print(colored(f'ПРОХОД {i + 1} НЕУДАЧНО', 'red')) # Вывод сообщения об ошибке при прохождении итерации
+                success = False # Установка значения success в False при ошибке записи в файл
+    try:
+        os.remove(path) # Удаление файла после перезаписи его содержимого случайными данными passes раз.
+        print(colored('ФАЙЛ УСПЕШНО УДАЛЕН.', 'green')) # Вывод сообщения об успешном удалении файла.
+    except: # Обработка исключений при удалении файла.
+        print(colored('ОШИБКА УДАЛЕНИЯ ФАЙЛА. ФАЙЛ МОЖЕТ БЫТЬ УНИЧТОЖЕН.', 'red')) 
+        success = False
 
-    # Открываем файл в режиме чтения и записи в бинарном режиме
-    with open(file_path, 'ba+') as f:
-        # Определяем размер файла
-        f.seek(0, os.SEEK_END)
-        file_size = f.tell()
-        # Перемещаем указатель в начало файла
-        f.seek(0)
-        # Перезаписываем файл случайными байтами
-        f.write(bytearray(random.getrandbits(8) for _ in range(file_size)))
-    # Удаляем файл с диска
-    os.remove(file_path)
+
+file = './vid.mp4' 
+
+print(colored('ВЫ УВЕРЕНЫ ЧТО ХОТИТЕ ПРОДОЛЖИТЬ С УНИЧТОЖЕНИЕМ ФАЙЛА? Y/N', 'yellow'))
+check = bool(input()) 
+
+if check: 
+    print(colored('УНИЧТОЖЕНИЕ ФАЙЛА', 'yellow'))
+    secure_delete(file) 
+    if success: 
+        print(colored('УСПЕСНО ЗАВЕРШЕН. Нажмите любую клавишу чтобы продолжить.', 'green'))
+        input()
+    else:
+        print(colored('Неудача одного или нескольких элементов. Пожалуйста попробуйте еще раз.', 'red'))
+        input()
