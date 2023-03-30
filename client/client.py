41147d6cd5f8029027bda36c2ef30e504f289bf0
Borisevich V <vasily.borisevich@gmail.com>
2023-03-30 11:12:47 +0300

    ASYNC!

--- client/client.py
+++ client/client.py
@@ -189,7 +189,7 @@ if (debug):
 # Подключение к серверу
 i = 1
 while i <= recon:
-    print(f'{i} попытка подключения к серверу...')
+    print(f'{i} попытка подключения к {server_IP}:{server_PORT}')
     try:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
