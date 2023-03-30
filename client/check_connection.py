85e399335bef4aae2162e4ba9922385345f8dba6
Borisevich V <vasily.borisevich@gmail.com>
2023-03-30 12:35:49 +0300

    Файл проверки доступности сервера

---
+++ client/check_connection.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import random
+import socket
+import configparser
+from sys import argv
+
+# Настройки
+server_IP = '10.0.20.200'    # Адрес сервера
+server_PORT = 49999          # Порт сервера
+recon = 5       # Количество попыток переподключения к серверу
+sa = argv[1:]   # Получение аргументов для запуска скрипта
+
+# Перебор ключени и работа с ними
+if (sa != []):
+    i = 0
+    while i < len(sa):
+        # если аргумент --port или -p, то меняем порт на указанный
+        if ((sa[i] == '--port') or (sa[i] == '-p')):
+            server_PORT = int(sa[i+1])
+
+        # То же самое для IP
+        if (sa[i] == '--ip'):
+            server_IP = sa[i+1]
+
+        if (sa[i] == '--recon'):
+            recon = int(sa[i+1])
+
+        i += 1
+
+# Подключение к серверу
+i = 1
+while i <= recon:
+    print(f'{i} попытка подключения к {server_IP}:{server_PORT}')
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        server.connect((server_IP, server_PORT))
+
+        running = True
+        print('Соединение установленно!')
+        break
+    except:
+        con = False
+
+    i += 1
+
+if (con == False):
+    print('Подключение к серверу отсутствует')
+
+input('Нажмите Enter для выхода')
