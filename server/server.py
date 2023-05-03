f689fbaf8fa3ce5e026d1f2c4e586a9bae660945
Borisevich V <vasily.borisevich@gmail.com>
2023-05-03 11:22:28 +0300

    fix

--- server/server.py
+++ server/server.py
@@ -260,6 +260,10 @@ if(rewrite):
 # Запуск сервера
 main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET работа с IPv4, SOCK_STREAM работа с TCP
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1) # Указывает на то, что не надо отправлять собранные пакеты, а слать информацию сразу
+main_socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 1)
+main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 3)
+main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 5)
 main_socket.bind((server_IP, server_PORT)) # Завязка сокета с IP и портом
 main_socket.setblocking(0) # Не останавливать выполнение программы пока получаем данные
 main_socket.listen(5) # Включение прослушки порта. 5 говорит о количестве одновременно подключаемых клиентов. 5 не максимальное количество клиентов!
@@ -298,7 +302,7 @@ async def main():
                 new_client = Lastochka(client_socket, client_addr) # Создаём нового клиента
                 client_list.append(new_client) # Записываем клиента в список
 
-            except:
+            except Exception as e:
                 pass
 
             # Чтение сообщений от клиентов
